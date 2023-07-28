bde72a8293f32fa70e638df58d6cd27534832124
gabriellend <gdvdsn@gmail.com>
2023-07-27 14:47:44 -1000

    Prevent red outlines on required inputs on page load

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // ELEMENTS
 const createAccountButton = document.querySelector("button");
 const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
 
 // FUNCTIONS
 const submitForm = (e) => {
   if (form.checkValidity()) {
+    inputs.forEach((input) => input.classList.remove("not-submitted"));
     e.preventDefault();
   }
 };
